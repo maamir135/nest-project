30cedf74aad79b40f30fb557914bdfe87553f294
Aamir <amir.manzoor@immcorporate.com>
2024-08-09 21:12:52 +0500

    added service for user

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,13 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Entity } from 'typeorm';
 import { User } from './models/user.entity';
+import { UserService } from './user.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User])
   ],
-  controllers: [UserController]
+  controllers: [UserController],
+  providers: [UserService]
 })
 export class UserModule {}
