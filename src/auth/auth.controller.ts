28ef79e7d0cd643dbfee438e38ed6d755a35d634
Aamir <amir.manzoor@immcorporate.com>
2024-08-11 17:07:22 +0500

    added validation for input data

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import path from 'path';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcryptjs';
+import { RegisterDto } from './models/register.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -11,7 +12,11 @@ export class AuthController {
     }
 
     @Post('register')
-    async register(@Body() body) {
+    async register(@Body() body: RegisterDto) {
+        if(body.password !== body.password_confirm){
+            throw new BadRequestException('Password do not match');
+        }
+
         const hashed = bcrypt.hash(body.password,12);
         return this.userService.create({
             first_name: body.first_name,
