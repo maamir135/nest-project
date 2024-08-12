8734f90ea6f567fa6f4c8caeabedc94041fdbf80
Aamir <amir.manzoor@immcorporate.com>
2024-08-12 15:44:48 +0500

    paginate users data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, Post, Put, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, Post, Put, Query, UseGuards, UseInterceptors } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './models/user.entity';
 import * as bcrypt from 'bcryptjs';
@@ -16,8 +16,8 @@ export class UserController {
     }
 
     @Get()
-    async all(): Promise<User[]> {
-        return this.userService.all();
+    async all(@Query('page') page: number =1): Promise<User[]> {
+        return this.userService.paginate();
     }
 
     @Post()
